4fe0c7b0a8498b6d84416032e0dd4f7a25492c4a
RaymovMardon <googlaccnew12@gmail.com>
2025-08-30 23:32:09 +0500

    edit header  30.08.25 23:32

--- src/app/Components/header/index.tsx
+++ src/app/Components/header/index.tsx
@@ -1,23 +1,39 @@
+"use client"
+import { useState } from "react"
 import Link from "../../../../node_modules/next/link"
-
+import style from "./header.module.scss"
 const Header = () => {
+  const [kategory,setKategory]= useState<boolean>(false);
   return (
     <>
     <header>
         <div className="custom_container flex items-center justify-between gap-4">
 
 <div><Link href={"/home"}><span> NextTV</span></Link></div>
-<nav className="my-5">
+<nav className="header_nav">
     <ul className="flex gap-4 ">
         <li>Home</li>
         <li>Channel</li>
-        <li>Kategory</li>
-        <li>Favorite</li>
+        <li onClick={()=>setKategory(!kategory)}>Kategory</li>
+        
         
     </ul>
 </nav>
 <div>lng</div>
         </div>
+        {kategory?(
+        <div className={style.kateegory_btn}>
+        <ul className="flex items-center justify-center gap-4 ">
+        <li>Uzb</li>
+        <li>Спортивные</li>
+        <li>Музыкальные</li>
+        <li>Кино</li>
+        <li>Новостные</li>
+        <li>Познавательные</li>
+        <li>Развлекательные</li>
+        <li>Детские</li>
+    </ul>
+        </div>):("")}
     </header>
     </>
   )
